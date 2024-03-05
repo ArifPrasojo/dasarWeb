c008c1ebdfe3bfbe0fa01fdbe77a132c3d6f0224
ArifPrasojo <arifprasojo999@gmail.com>
2024-03-05 12:42:45 +0700

    Praktikum 1 - Pertemuan 4

---
+++ Pertemuan4/variabel_konstanta .php
@@ -0,0 +1,15 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "Hasil Perjumlahan $angka1 dan $angka2 adalah $hasil .";
+
+$benar = true;
+$salah = false;
+echo "Variabel benar : $benar, Varibael Salah : $salah";
+
+define("NAMA_SITUS", "Websiteku.com");
+define("TAHUN_PENDIRIAN", 2023);
+
+echo "Selamat datang di ". NAMA_SITUS.",situs yang didirikan pada tahun". TAHUN_PENDIRIAN.".";
+?>
