b6d755d2f07880fdf4bda7203f67de0ec3b23199
ArifPrasojo <arifprasojo999@gmail.com>
2024-03-19 11:56:56 +0700

    Pertemuan 5 - Praktikum 1

--- Pertemuan5/array_1.php
+++ Pertemuan5/array_1.php
@@ -2,10 +2,23 @@
 <html>
     <head></head>
     <body>
-        <h2>Array Terindeks</h2>
+        <h2>Array Terindeks dan Perulangan</h2>
         <?php
         $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
 
+        // Perulangan For
+        for ($i = 0; $i < count($Listdosen); $i++) {
+            echo $Listdosen[$i] . "<br>";
+        }
+        echo "<br>";
+
+        // Perulangan Foreach
+        foreach($Listdosen as $dosen){
+            echo $dosen . "<br>";
+        }
+        echo "<br>";
+
+        //Index
         echo $Listdosen[2] . "<br>";
         echo $Listdosen[0] . "<br>";
         echo $Listdosen[1] . "<br>";
